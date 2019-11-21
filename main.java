253d7a35b4d88cb750b9b8f5e78a89d0aa048f4b
colincassens <ccassens@purdue.edu>
2019-11-21 12:35:27 -0500

    IO
    Read in the file by line and determine if the line was a comment

--- main.java
+++ main.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class Main{
     public static void main(String args[]){
@@ -12,12 +14,20 @@ class Main{
             String line = reader.readLine();
             while(line != null){
                 //CALL FUNC TO TAKE ACTION ON THE LINE
-                System.out.println(line);
+                LineReader(line);
                 //READ THE NEXT LINE
                 line = reader.readLine();
             }
         }catch (IOException e){
             e.printStackTrace();
+        }
+    }
+
+    public static void LineReader(String line){
+        //Determines if the line is a comment or not
+        boolean comment = line.indexOf("//") != -1? true: false;
+        if(!comment){
+            System.out.println(line);
         }
     }
 }
