0e2ff38b25166d642ff173f0ee0ee39a0d30c5a7
colincassens <ccassens@purdue.edu>
2019-12-02 15:49:58 -0500

    Added Language/(Funcs).java

--- main.java
+++ main.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import Language.*;
 
 class Main{
     public static void main(String args[]){
