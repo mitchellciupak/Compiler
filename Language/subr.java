e63291bfd34bbd5065754451835c4b79f7c496f0
Mitchell Ciupak <laggant@gmail.com>
2019-12-03 10:27:11 -0500

    subr - hardcoded

--- Language/subr.java
+++ Language/subr.java
@@ -1,4 +1,42 @@
 package Language;
 
+import java.nio.ByteBuffer;
+
 public class subr {
+    //pushi 3 times (16,17,1)
+    // call
+    // halt
+    public static void execute(String[] parsed){
+        //pushi 16
+        char c = 70;
+        FileOutput.write(c);
+
+        int i = 16; //Integer.parseInt(parsed[1]);
+        byte[] x = ByteBuffer.allocate(4).putInt(i).array();
+        FileOutput.write(x);
+
+        //pushi 16
+        char c = 70;
+        FileOutput.write(c);
+
+        int i = 17; //Integer.parseInt(parsed[1]);
+        byte[] x = ByteBuffer.allocate(4).putInt(i).array();
+        FileOutput.write(x);
+
+        //pushi 1
+        char c = 70;
+        FileOutput.write(c);
+
+        int i = 1; //Integer.parseInt(parsed[1]);
+        byte[] x = ByteBuffer.allocate(4).putInt(i).array();
+        FileOutput.write(x);
+
+        //call
+        c = 44;
+        FileOutput.write(c);
+
+        //halt
+        c = 0;
+        FileOutput.write(c);
+    }
 }
