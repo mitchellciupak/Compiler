00e1db9b0cfb29c508a15e65d5598cc76a3ff283
Mitchell Ciupak <laggant@gmail.com>
2019-12-02 21:57:04 -0500

    getter update

--- Getter.java
+++ Getter.java
@@ -4,65 +4,66 @@ public class Getter {
     public static void chooseCode(String[] parsed){
         String buffer = parsed[0];
 
-        if (buffer == "halt") {
-            Halt.print(parsed);
-            exit(0);
-        }
-
         switch (buffer) {
-            case "jmp": //36
-                jmp.execute(parsed);
-                break;
-            case "jmpc": //40
-                jmpc.execute(parsed);
-                break;
-            case "pushi": //70
-                pushi.execute(parsed);
-                break;
-            case "pushvi": //74
-                pushvi.execute(parsed);
-                break;
-            case "popm": //76
-                popm.execute(parsed);
-                break;
-            case "popa": //77
-                popa.execute(parsed);
-                break;
-            case "popv": //80:
-                popv.execute(parsed);
-                break;
-            case "peeki": //86:
-                peeki.execute(parsed);
-                break;
-            case "pokei": //90:
-                pokei.execute(parsed);
-                break;
-            case "swp": //94:
-                swp.execute(parsed);
-                break;
-            case "add": //100:
+            case "add":
                 add.execute(parsed);
                 break;
-            case "sub": //104:
-                sub.execute(parsed);
-                break;
-            case "mul": //108:
-                mul.execute(parsed);
-                break;
-            case "div": //112:
-                div.execute(parsed);
-                break;
-            case "cmpe": //132:
+            case "cmpe":
                 cmpe.execute(parsed);
                 break;
-            case "cmplt": //136:
-                cmplt.execute(parsed);
-                break;
-            case "cmpgt": //140:
+            case "cmpgt":
                 cmpgt.execute(parsed);
                 break;
-            case "printi": //146:
-                printi.execute(parsed);
+            case "cmplt":
+                cmplt.execute(parsed);
+                break;
+            case "decl":
+                decl.execute(parsed);
+                break;
+            case "div":
+                div.execute(parsed);
+                break;
+            case "jmp":
+                jmp.execute(parsed);
+                break;
+            case "jmpc":
+                jmpc.execute(parsed);
+                break;
+            case "lab":
+                lab.execute(parsed);
+                break;
+            case "mul":
+                mul.execute(parsed);
+                break;
+            case "peek":
+                peek.execute(parsed);
+                break;
+            case "poke":
+                poke.execute(parsed);
+                break;
+            case "popm":
+                popm.execute(parsed);
+                break;
+            case "popv":
+                popv.execute(parsed);
+                break;
+            case "print":
+                print.execute(parsed);
+                break;
+            case "printv":
+                printv.execute(parsed);
+                break;
+            case "push":
+                push.execute(parsed);
+                break;
+            case "sub":
+                sub.execute(parsed);
+                break;
+            case "subr":
+                subr.execute(parsed);
+                break;
+            case "swp":
+                swp.execute(parsed);
                 break;
             default:
                 System.out.println("Invalid");
