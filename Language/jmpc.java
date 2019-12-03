5e41ae838a6c90abd7241f7107917a4af4f27019
Mitchell Ciupak <laggant@gmail.com>
2019-12-03 00:39:55 -0500

    partial peek,poke,jmp,jmpc

--- Language/jmpc.java
+++ Language/jmpc.java
@@ -1,4 +1,14 @@
 package Language;
 
 public class jmpc {
+    public static void execute(String[] parsed){
+        //pushi (label)
+        char c = 70;
+        FileOutput.write(c);
+        //TODO (label)
+
+        //bc.jmpc
+        c = 40;
+        FileOutput.write(c);
+    }
 }
--- Language/peek.java
+++ Language/peek.java
@@ -1,4 +1,37 @@
 package Language;
 
+import java.nio.ByteBuffer;
+
 public class peek {
+    public static void execute(String[] parsed) {
+        //pushi (var)
+        char c = 70;
+        FileOutput.write(c);
+        //TODO (var)
+//        int i = 0;
+//        byte[] x = ByteBuffer.allocate(4).putInt(i).array();
+//        FileOutput.write(x);
+
+        //pushi val
+        c = 70;
+        FileOutput.write(c);
+        //TODO (val)
+
+        //bc.peek<type>
+        //todo find <type>
+        char type = 'i';
+        if(type == 'c'){ //char
+            c = 84;
+        }
+        else if(type == 's') { //short
+            c = 85;
+        }
+        else if(type == 'i') { //int
+            c = 86;
+        }
+        else if(type == 'f') { //float
+            c = 87;
+        }
+        FileOutput.write(c);
+    }
 }
